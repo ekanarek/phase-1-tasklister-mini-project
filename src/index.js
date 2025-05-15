606ed2b56c3b7fc8e2d9dc804962d9068a3dc7f3
Emily Kanarek <emilyakanarek@gmail.com>
2025-05-15 09:46:59 -0400

    Add and delete items in task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .querySelector("#create-task-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const newTask = document.createElement("li");
+      newTask.textContent = document.querySelector(
+        "input#new-task-description"
+      ).value;
+      newTask.id = "task";
+      document.querySelector("ul#tasks").append(newTask);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "x";
+      deleteButton.addEventListener("click", () => newTask.remove());
+
+      newTask.append(deleteButton);
+      document.querySelector("input#new-task-description").value = "";
+    });
 });
